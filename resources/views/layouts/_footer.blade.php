5400bb8fa756f277f0ab5d4d634f850e204c138f
awen0317 <awen0317.163.com>
2019-09-11 05:53:23 +0000

    提交用户注册页面之前的代码

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+ <a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="{{ route('about') }}" >关于</a>
+    </div>
+</footer>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
+@stop
