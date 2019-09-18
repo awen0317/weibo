6312465e18434b649fcb65a95f3ee7cde2d9d9fd
awen0317 <awen0317.163.com>
2019-09-18 12:26:14 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
