5791089e37b03ec0ffafe6c2901f1182649c6dde
awen0317 <awen0317.163.com>
2019-09-23 03:10:36 +0000

    提交代码

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+    public function feed(){
+        return $this->statuses()->orderBy('created_at','desc');
+    }
 }
